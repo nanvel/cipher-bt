96d3f1215289988bc1a0d2f2be788e90bbea9d0a
Oleksandr <1374878+nanvel@users.noreply.github.com>
2023-01-29 15:40:13 +0700

    Create README on init (#23)

--- cipher/use_cases/init_repository.py
+++ cipher/use_cases/init_repository.py
@@ -2,13 +2,22 @@ from pathlib import Path
 
 
 class InitRepository:
-    """Create .env."""
+    """Create .env, README.md."""
 
     def __init__(self, templates_root: Path):
         self.templates_root = templates_root
 
     def call(self, path: Path):
-        dot_env_path = path / ".env"
-        if not dot_env_path.exists():
-            with dot_env_path.open("w", encoding="utf-8") as f:
-                f.write((self.templates_root / "env.j2").read_text())
+        self._create_file_from_template(
+            template_path=self.templates_root / "env.j2",
+            target_path=path / ".env",
+        )
+        self._create_file_from_template(
+            template_path=self.templates_root / "readme.j2",
+            target_path=path / "README.md",
+        )
+
+    def _create_file_from_template(self, template_path: Path, target_path: Path):
+        if not target_path.exists():
+            with target_path.open("w", encoding="utf-8") as f:
+                f.write(template_path.read_text())
