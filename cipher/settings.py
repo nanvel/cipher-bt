317ce5f7137093e4a1a3391207743e122d4b36d2
Oleksandr Polieno <polyenoom@gmail.com>
2025-08-16 09:48:51 +0700

    Ignore extra settings

--- cipher/settings.py
+++ cipher/settings.py
@@ -6,7 +6,11 @@ from cipher.models import LogLevel
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore",
+    )
 
     cache_root: Path = ".cache"
     log_level: LogLevel = LogLevel.INFO
